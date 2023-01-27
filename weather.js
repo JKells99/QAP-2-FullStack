99ef79e1315e8e2bba00b34a7794884e19f3c599
JKells99 <j_kelloway@hotmail.com>
2023-01-27 14:56:56 -0330

    attempted to use npm for weather data could not...

---
+++ weather.js
@@ -0,0 +1,31 @@
+// Attempted to use a npm package to figure out how to display the weather 
+
+// var weather = require('weather-js')
+
+// weather.find({search: 'St Johns, NL', degreeType: 'C'}, function(err, result) {
+//     if(err) console.log(err);
+    
+
+//     const json = JSON.stringify(result,null,2)
+
+
+    
+
+    
+    
+
+    
+
+   
+
+
+
+//     return json
+
+ 
+    
+//   });
+
+//   console.log(json)
+
+
